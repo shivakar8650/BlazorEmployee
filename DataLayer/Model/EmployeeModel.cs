ace390ece13f0e1e19c3af7c7ca3d845af5a74f1
shivakar <shivakar@ch.iitr.ac.in>
2022-01-21 00:53:54 +0530

    [shivakar]added:create Empregister,EmpDashbord and set connection string with Mongodb

---
+++ DataLayer/Model/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    public  class EmployeeModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string id { get; set; }
+        [Required(ErrorMessage = "Enter the name")]
+        public string name { get; set; }
+        public string ProfileImage { get; set; }
+        public int MyProperty { get; set; }
+        [Required(ErrorMessage ="Gender is missing")]
+        public string gender { get; set; }
+        [Required(ErrorMessage = "Deaprtment")]
+        public string Department  { get; set; }
+        public string Salary { get; set; }
+        public string SatrtDate { get; set; }
+        public string Note { get; set; }
+
+    }
+}
